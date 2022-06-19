f96830ca200cad513b8d21ca6337ccc0ea69b558
kailee-madden <40588977+kailee-madden@users.noreply.github.com>
2022-06-19 14:24:53 -0700

    Update test_credentials.py

--- test_credentials.py
+++ test_credentials.py
@@ -3,13 +3,12 @@ import sys
 from spotipy.oauth2 import SpotifyClientCredentials
 import random
 
-client_credentials_manager = SpotifyClientCredentials(client_id="6c449b3966964f9e865d70a8709bf77f",
-                                                      client_secret="d107d61ca36345a98c3ac72d523875f9")
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, 
+                                                    client_secret=CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-
-username="kailee.madden"
-playlist_id="4kOz4MxGsxuWLmP4ZaSsOz"
+username=USERNAME
+playlist_id=ID
 songs = {}
 results = sp.user_playlist(username, playlist_id, fields="tracks(items(track(id, name)))")
 for element in results['tracks']['items']:
